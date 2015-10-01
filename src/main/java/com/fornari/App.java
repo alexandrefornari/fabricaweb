3599969a847dd3dc9a15e7d0647d0b09c8a174f6
Alexandre <alexandrefornari@gmail.com>
2015-10-01 10:22:24 -0300

    Iniciando

---
+++ src/main/java/com/fornari/App.java
@@ -0,0 +1,13 @@
+package com.fornari;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
